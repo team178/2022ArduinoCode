12fa0f46130a4f0f03a9066d007bebb9bcb2bd21
vanden46 <vanden46@gmail.com>
2022-02-13 18:16:22 -0500

    Create 2022Robot Lights Base.ino

---
+++ 2022Robot Lights Base/2022Robot Lights Base.ino
@@ -0,0 +1,23 @@
+#include <Adafruit_NeoPixel.h>
+
+/*
+Decimal	  Binary(pin/code)			        Action	
+	        DIO7/do0	DIO8/do1	DIO9/do2		
+0	        0	    	  0		      0		      Blue Alliance Normal	
+1	        0	    	  0	      	1    	  	Blue Shoot	
+2	        0		      1		      0	      	Blue Climb	
+3	        0	    	  1		      1	      	Red Alliance Normal	
+4	        1		      0		      0     		Red Shoot	
+5	        1		      0		      1	      	Red Climb	
+6         1         1         0         Enforcers
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
