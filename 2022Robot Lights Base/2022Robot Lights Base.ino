6a667f8c3e03af88c665cd236114af9fee7d581d
vanden46 <vanden46@gmail.com>
2022-02-13 19:28:18 -0500

    Update 2022Robot Lights Base.ino

--- 2022Robot Lights Base/2022Robot Lights Base.ino
+++ 2022Robot Lights Base/2022Robot Lights Base.ino
@@ -1,4 +1,8 @@
 #include <Adafruit_NeoPixel.h>
+#define NUM 48//we need to determine how many pixels we are using, this may change
+#define PIN 4
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM, PIN, NEO_GRB + NEO_KHZ800);
+
 
 /*
 Decimal	  Binary(pin/code)			        Action	
@@ -12,9 +16,57 @@ Decimal	  Binary(pin/code)			        Action
 6         1         1         0         Enforcers
 */
 
+//input pins
+int do0 = 7;
+int do1 = 8;
+int do2 = 9;
+
+//stored pins
+bool d0=1;
+bool d1=1;
+bool d2=0;
+
+
+//Enforcers Colors
+int enforcerBlue = strip.Color(2, 5, 121);
+int enforcerYellow = strip.Color(255,208,0);
+//Allience Colors
+int red = strip.Color(237, 28, 36);
+int blue = strip.Color(0, 101, 179);
+
+void blueNormal(){
+
+}
+
+void blueShoot(){
+
+}
+
+void blueClimb(){
+
+}
+
+void redNormal(){
+
+}
+
+void redShoot(){
+
+}
+
+void redClimb(){
+  
+}
+
+void enforcers(){
+
+}
+
+
+
 void setup() {
   // put your setup code here, to run once:
-  
+
 }
 
 void loop() {
