056187ceaedf9fd8f416c56bc4da789c45575266
Team178Laptop <programming@farmingtonrobotics.org>
2022-02-16 17:36:04 -0500

    Update 178.ino
    sai

--- Sai/178/178.ino
+++ Sai/178/178.ino
@@ -16,6 +16,8 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   show1();
+  show7();
+  show8();
 }
 
 
@@ -27,9 +29,28 @@ void show1(){
 }
 
 void show7(){
-  
+  strip.setPixelColor(35, 0,0,255);
+  strip.setPixelColor(36, 0,0,255);
+  strip.setPixelColor(59, 0,0,255);
+  strip.setPixelColor(60, 0,0,255);
+  for(int x=72; x<= 83; x++){
+  strip.setPixelColor(x, 0,0,255);
+  }
+  strip.show();
 }
 
 void show8(){
-  
+  for( int x = 96; x<=107; x++){
+    strip.setPixelColor(x,0,0,255);
+  }
+  strip.setPixelColor(108,0,0,255);
+  strip.setPixelColor(119,0,0,255);
+  strip.setPixelColor(120,0,0,255);
+  strip.setPixelColor(125,0,0,255);
+  strip.setPixelColor(114,0,0,255);
+  strip.setPixelColor(131,0,0,255);
+  for(int x = 132; x<144; x++){
+    strip.setPixelColor(x,0,0,255);
+  }
+  strip.show();
 }
