797e5f17625d994a55bea403cacc835595e6286c
sai-raparla <sairaparla@gmail.com>
2022-03-11 09:08:31 -0500

    Sai - Shooter

---
+++ Sai/Climber/Climber.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+
+  //just to comit
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ Sai/Shooter/Shooter.ino
@@ -0,0 +1,34 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 4	 // input pin Neopixel is attached to
+
+#define NUMPIXELS      144 // number of neopixels in strip
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+int delayval = 100; // timing delay in milliseconds
+
+int redColor = 0;
+int greenColor = 0;
+int blueColor = 0;
+
+void setup() {
+  // Initialize the NeoPixel library.
+  pixels.begin();
+}
+
+void loop() {
+  setColor();
+}
+
+// setColor()
+// picks random values to set for RGB
+void setColor(){
+  redColor = random(0, 255);
+  greenColor = random(0,255);
+  blueColor = random(0, 255);
+  for (int i=0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(redColor, greenColor, blueColor));
+  }
+  pixels.show();
+}
